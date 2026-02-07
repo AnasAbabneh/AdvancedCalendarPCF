d700d2e7e0e13639573a12a0d09c0ab0ce68bc78
Anas <60849108+AnasAbabneh@users.noreply.github.com>
2026-02-07 17:08:42 +0300

    v1

---
+++ components/Main/MainComponent.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export const MainComponent = (props: any) => {
+    return (
+        <div style={{ padding: "12px", border: "1px solid #ccc", borderRadius: "6px" }}>
+            <h3>Advanced Calendar PCF with multi-date and range selection</h3>
+            <p>This is a PCF starter component.</p>
+        </div>
+    );
+};
